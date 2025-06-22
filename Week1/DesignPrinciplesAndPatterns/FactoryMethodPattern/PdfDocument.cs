50e290fc1180717b4d023ef0c8d3810b7186629d
Sweta Singh <93272237+SwetaSingh18217@users.noreply.github.com>
2025-06-22 20:56:31 +0530

    Add files via upload

---
+++ Week1/DesignPrinciplesAndPatterns/FactoryMethodPattern/PdfDocument.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class PdfDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Opening a PDF document.");
+    }
+}
+
---
+++ Week1/DesignPrinciplesAndPatterns/SingletonPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+ 
+        Logger logger1 = Logger.GetInstance();
+        logger1.Log("This is the first log message.");
+
+    
+        Logger logger2 = Logger.GetInstance();
+        logger2.Log("This is the second log message.");
+
+       
+        if (logger1 == logger2)
+        {
+            Console.WriteLine("logger1 and logger2 are the same instance.");
+        }
+        else
+        {
+            Console.WriteLine("logger1 and logger2 are different instances.");
+        }
+    }
+}
+
